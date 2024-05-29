6b843e179e012b52ec56fa1bd42d13023cce7a4a
nazarboy <rakhmonovnazarboy0@gmail.com>
2024-05-29 22:27:47 +0500

    product details

--- blogs/views.py
+++ blogs/views.py
@@ -1,31 +1,38 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from blogs.models import BlogModel, BlogCategoryModel, BlogTagModel
 
 
 # Create your views here.
-class BlogListView(TemplateView):
-    template_name = 'blogs/blog-list.html'
+from django.views.generic import ListView
+from .models import BlogModel, BlogCategoryModel, BlogTagModel
 
-    def get_context_data(self, **kwargs):
+
+class BlogListView(ListView):
+    template_name = 'blogs/blog-list.html'
+    context_object_name = 'blogs'
+    paginate_by = 2
+
+    def get_queryset(self):
         blogs = BlogModel.objects.all().order_by('-created_at')
         cat = self.request.GET.get('cat')
         tag = self.request.GET.get('tag')
 
         if cat:
-            blogs = blogs.filter(categories=cat)
+            blogs = blogs.filter(categories__id=cat)  # Assuming categories and tags are foreign keys
         if tag:
-            blogs = blogs.filter(tags=tag)
+            blogs = blogs.filter(tags__id=tag)
 
+        return blogs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         categories = BlogCategoryModel.objects.all()
-        famous_posts = BlogModel.objects.all().order_by()[:2]
+        famous_posts = BlogModel.objects.all().order_by('-created_at')[:2]
         tags = BlogTagModel.objects.all()
-        context = {
-            'categories': categories,
-            'blogs': blogs,
-            'famous_posts': famous_posts,
-            'tags': tags,
-        }
 
+        context['categories'] = categories
+        context['famous_posts'] = famous_posts
+        context['tags'] = tags
         return context
 
 
--- shit-to-be-deleted.py
+++ shit-to-be-deleted.py
@@ -1,8 +1,9 @@
 numbers = [f'n{i}' for i in range(10)]
 print(numbers)
+nums = []
+for number in numbers[:-1]:
+    if numbers.index(number) % 2 == 0:
+        nums.append([number, numbers[numbers.index(number) + 1]])
+print(nums)
 
-for number in numbers:
-    if numbers.index(number)%2 != 1:
-        print(number)
-        print(numbers[numbers.index(number) + 1])
 
